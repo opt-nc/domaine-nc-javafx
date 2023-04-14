c00d4136a30827deea0db912d25be3c5ce4ecd11
Kevin-Petit <kevin.petit2301@gmail.com>
2023-04-14 11:06:17 +1100

    Debut de style (CSS)

--- src/main/java/com/unc/domainenc/HelloApplication.java
+++ src/main/java/com/unc/domainenc/HelloApplication.java
@@ -16,6 +16,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(HelloApplication.class.getResource("css/main.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
