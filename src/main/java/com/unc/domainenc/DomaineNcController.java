f159dca9a1db280a1b0123189c11f3b2d237054b
Kevin-Petit <kevin.petit2301@gmail.com>
2023-05-12 13:41:37 +1100

    Ajout affichage d'une nouvelle fenêtre lors d'un clique sur un Domaine

--- src/main/java/com/unc/domainenc/DomaineNcController.java
+++ src/main/java/com/unc/domainenc/DomaineNcController.java
@@ -5,11 +5,16 @@ import com.unc.domainenc.api.DomaineEntity;
 import com.unc.domainenc.api.Request;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,6 +44,7 @@ public class DomaineNcController {
     @FXML
     protected void onClick() {
         searchButton.setDisable(true);
+        searchBar.setDisable(true);
         vBoxDomaines.getChildren().clear();
         Thread searchThread = new Thread(() -> {
             List<DomaineEntity> sortList = searchList(searchBar.getText());
@@ -49,15 +55,33 @@ public class DomaineNcController {
                     throw new RuntimeException(e);
                 }
                 Platform.runLater(() -> {
-                    TextField textField = new TextField(String.format("Name: %s , Extensions: %s", domaine.getName(), domaine.getExtension()));
+                    TextField textField = new TextField(String.format("Nom: %s , Extensions: %s", domaine.getName(), domaine.getExtension()));
                     textField.setEditable(false);
+                    textField.setOnMouseClicked(this::onTextFieldClick);
                     vBoxDomaines.getChildren().add(textField);
                 });
             }
             searchButton.setDisable(false);
+            searchBar.setDisable(false);
         });
         searchThread.setDaemon(true);
         searchThread.start();
+    }
+
+    public void onTextFieldClick(MouseEvent event) {
+        TextField textField = (TextField) event.getSource();
+        String[] listContenu = textField.getText().split(" ");
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(DomaineNcApp.class.getResource("InterfaceInfoDomaineNc.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 420, 440);
+            scene.getStylesheets().add(DomaineNcApp.class.getResource("css/StyleDomaineNc.css").toExternalForm());
+            stage.setTitle(listContenu[1] + listContenu[4]);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     protected List<DomaineEntity> searchList(String searchWords) {
