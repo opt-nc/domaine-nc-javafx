76cdcef08ee64efefd6c21afee4fbcdff6d46495
SALES <Adrien.Sales@gmail.com>
2023-05-25 08:13:04 +1100

    Update DomaineNcApp.java

--- src/main/java/com/unc/domainenc/DomaineNcApp.java
+++ src/main/java/com/unc/domainenc/DomaineNcApp.java
@@ -20,17 +20,25 @@ public class DomaineNcApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        String apiKEY = System.getenv("X-RAPIDAPI-KEY");
+        // TODO Positionner la clé "X_RAPIDAPI_KEY" statiue en variable final et passer par la vaiable au lieu de la code en dur partout
+        String apiKEY = System.getenv("X_RAPIDAPI_KEY");
+        // TODO AJouter de la log pour dire ce qui est fait comme recherche, très utile... "quand ça ne marche pas" et qu'il faut gérer un enduser
         if (apiKEY == null) {
             try {
                 Dotenv dotenv = Dotenv.load();
-                apiKEY = dotenv.get("X-RAPIDAPI-KEY");
+                apiKEY = dotenv.get("X_RAPIDAPI_KEY");
                 if (apiKEY == null) {
-                    JOptionPane.showMessageDialog(null, "Erreur:\n.env Incorect\nX-RAPIDAPI-KEY non trouver.", "Erreur", JOptionPane.ERROR_MESSAGE);
+                    // TODO styler la fenêtre avec le thème de l'app
+                    // cf https://github.com/adriens/domaine-nc-javafx/issues/20#issuecomment-1561907166
+                    JOptionPane.showMessageDialog(null, "Erreur:\n.env Incorect\nX_RAPIDAPI_KEY non trouvée.", "Erreur", JOptionPane.ERROR_MESSAGE);
+                    // TODO : Ajouter de la log console pour dire ce qu'il faut faire pour que ça marche
+                    // cf https://github.com/adriens/domaine-nc-javafx/issues/20#issuecomment-1561907166
                     System.exit(1);
                 }
             } catch (DotenvException e) {
-                JOptionPane.showMessageDialog(null, "Erreur:\nImpossible de trouver X-RAPIDAPI-KEY.", "Erreur", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Erreur:\nImpossible de trouver X_RAPIDAPI_KEY.", "Erreur", JOptionPane.ERROR_MESSAGE);
+                // TODO : Ajouter de la log console pour dire ce qu'il faut faire pour que ça marche
+                // cf https://github.com/adriens/domaine-nc-javafx/issues/20#issuecomment-1561907166
                 System.exit(1);
             }
         }
@@ -38,6 +46,8 @@ public class DomaineNcApp extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(DomaineNcApp.class.getResource("InterfaceDomaineNc.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 420, 440);
         scene.getStylesheets().add(Objects.requireNonNull(DomaineNcApp.class.getResource("css/StyleDomaineNc.css")).toExternalForm());
+        
+        // Passer par une varibale globale (ie. ne pas coder en dur ce genre contenu en local dans une méthode... sinon c pas facile à maintenir)
         stage.setTitle("DOMAINE.nc");
         stage.getIcons().add(new Image(Objects.requireNonNull(DomaineNcApp.class.getResourceAsStream("img/Icon.png"))));
         stage.setScene(scene);
