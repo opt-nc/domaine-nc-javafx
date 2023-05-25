a77da243603f460ba1c0f8795fd814e1d10060fb
Kevin-Petit <kevin.petit2301@gmail.com>
2023-05-25 19:11:42 +1100

    Logs + Update message d'erreur

--- src/main/java/com/unc/domainenc/DomaineNcApp.java
+++ src/main/java/com/unc/domainenc/DomaineNcApp.java
@@ -1,55 +1,99 @@
 package com.unc.domainenc;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.util.StatusPrinter;
 import com.unc.domainenc.api.Request;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.util.Objects;
 
 public class DomaineNcApp extends Application {
+
+    public static final String os = System.getProperty("os.name").toLowerCase();
+    private static final Logger logger = LoggerFactory.getLogger(DomaineNcApp.class);
+    public static Image appIcon = new Image(Objects.requireNonNull(DomaineNcApp.class.getResourceAsStream("img/Icon.png")));
+    public static String appStyle = Objects.requireNonNull(DomaineNcApp.class.getResource("css/StyleDomaineNc.css")).toExternalForm();
+    private final String apiKEYname = "X_RAPIDAPI_KEY";
+
     public static void main(String[] args) {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.reset();
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(loggerContext);
+        try {
+            configurator.doConfigure(DomaineNcApp.class.getResourceAsStream("logback.xml"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        StatusPrinter.printInCaseOfErrorsOrWarnings(loggerContext);
+        logger.info("Lancement de l'application.");
         launch();
+    }
+
+    public static void afficheError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erreur");
+        alert.setHeaderText(null);
+        alert.setContentText("Erreur:\n" + message);
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.getStylesheets().add(DomaineNcApp.appStyle);
+        dialogPane.getStyleClass().add("color");
+        alert.showAndWait();
     }
 
     @Override
     public void start(Stage stage) throws IOException {
-        // TODO Positionner la clé "X_RAPIDAPI_KEY" statiue en variable final et passer par la vaiable au lieu de la code en dur partout
-        String apiKEY = System.getenv("X_RAPIDAPI_KEY");
-        // TODO AJouter de la log pour dire ce qui est fait comme recherche, très utile... "quand ça ne marche pas" et qu'il faut gérer un enduser
+        logger.info("Recherche de la clef d'API dans l'environement du systeme.");
+        String apiKEY = System.getenv(apiKEYname);
         if (apiKEY == null) {
+            logger.warn("Clef d'API non trouvee dans l'environement du systeme.");
             try {
+                logger.info("Recherche de la clef d'API dans le fichier .env.");
                 Dotenv dotenv = Dotenv.load();
-                apiKEY = dotenv.get("X_RAPIDAPI_KEY");
+                apiKEY = dotenv.get(apiKEYname);
                 if (apiKEY == null) {
-                    // TODO styler la fenêtre avec le thème de l'app
-                    // cf https://github.com/adriens/domaine-nc-javafx/issues/20#issuecomment-1561907166
-                    JOptionPane.showMessageDialog(null, "Erreur:\n.env Incorect\nX_RAPIDAPI_KEY non trouvée.", "Erreur", JOptionPane.ERROR_MESSAGE);
-                    // TODO : Ajouter de la log console pour dire ce qu'il faut faire pour que ça marche
-                    // cf https://github.com/adriens/domaine-nc-javafx/issues/20#issuecomment-1561907166
+                    logger.warn("Clef d'API non trouvee dans le fichier .env.");
+                    DomaineNcApp.afficheError(".env Incorect\n" + apiKEYname + " non trouvee.\n\n" +
+                            "Le .env dois contenir: " + apiKEYname + "=Votre_clef_d'API");
                     System.exit(1);
                 }
+                logger.info("Clef d'API trouvee dans le fichier .env.");
             } catch (DotenvException e) {
-                JOptionPane.showMessageDialog(null, "Erreur:\nImpossible de trouver X_RAPIDAPI_KEY.", "Erreur", JOptionPane.ERROR_MESSAGE);
-                // TODO : Ajouter de la log console pour dire ce qu'il faut faire pour que ça marche
-                // cf https://github.com/adriens/domaine-nc-javafx/issues/20#issuecomment-1561907166
+                logger.warn("Fichier .env introuvable.");
+                String errorMsg = "Impossible de trouver " + apiKEYname + ".\n\n" +
+                        "Creer un fichier .env qui dois contenir: " + apiKEYname + "=Votre_clef_d'API\n" +
+                        "ou\n" +
+                        "Ajouter votre clef d'API dans l'environement du systeme.\n\n";
+                if (os.contains("windows")) {
+                    DomaineNcApp.afficheError(errorMsg + "Windows:\nsetx " + apiKEYname + " \"Votre_clef_d'API\"");
+                } else if (os.contains("linux") || os.contains("mac")) {
+                    DomaineNcApp.afficheError(errorMsg + "Linux/Mac:\nexport " + apiKEYname + "=\"Votre_clef_d'API\"");
+                } else {
+                    DomaineNcApp.afficheError(errorMsg);
+                }
                 System.exit(1);
             }
+        } else {
+            logger.info("Clef d'API trouvee dans l'environement du systeme.");
         }
         Request.setApiKey(apiKEY);
         FXMLLoader fxmlLoader = new FXMLLoader(DomaineNcApp.class.getResource("InterfaceDomaineNc.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 420, 440);
-        scene.getStylesheets().add(Objects.requireNonNull(DomaineNcApp.class.getResource("css/StyleDomaineNc.css")).toExternalForm());
-        
-        // Passer par une varibale globale (ie. ne pas coder en dur ce genre contenu en local dans une méthode... sinon c pas facile à maintenir)
+        scene.getStylesheets().add(DomaineNcApp.appStyle);
         stage.setTitle("DOMAINE.nc");
-        stage.getIcons().add(new Image(Objects.requireNonNull(DomaineNcApp.class.getResourceAsStream("img/Icon.png"))));
+        stage.getIcons().add(DomaineNcApp.appIcon);
         stage.setScene(scene);
         stage.show();
     }
