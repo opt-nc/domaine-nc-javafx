ae7ed6864f5d2f54d172f9a5594db19e5c0da9fb
Davnox900 <domergudavid49@gmail.com>
2023-05-26 16:16:35 +1100

    création des deux lien

--- src/main/java/com/unc/domainenc/DomaineNcInfoController.java
+++ src/main/java/com/unc/domainenc/DomaineNcInfoController.java
@@ -4,6 +4,7 @@ import com.unc.domainenc.api.DomaineInfoEntity;
 import com.unc.domainenc.api.Request;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -13,6 +14,11 @@ import javafx.scene.layout.VBox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.awt.Desktop;
+import javafx.scene.input.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -24,6 +30,7 @@ public class DomaineNcInfoController implements Initializable {
     private static final Logger logger = LoggerFactory.getLogger(DomaineNcInfoController.class);
     private final String nom;
     private final Request request;
+    private String ridet;
     @FXML
     private VBox infoVbox;
 
@@ -36,7 +43,9 @@ public class DomaineNcInfoController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         logger.info(String.format("Recuperation des information sur " + nom + ".nc."));
         DomaineInfoEntity domaineInfoEntity = request.getDomaineInfo(nom);
-        addInfo("Bénéficiaire :\n" + cleanRidet(domaineInfoEntity.getBeneficiaire()), FontAwesomeIcon.CREDIT_CARD);
+        this.ridet = cleanRidet(domaineInfoEntity.getBeneficiaire());
+        addInfoLien("Ouvrir sur Domaine.nc", FontAwesomeIcon.GLOBE);
+        addInfoLienRidet("Bénéficiaire :\n" + ridet, FontAwesomeIcon.CREDIT_CARD);
         addInfo("Gestionnaire :\n" + domaineInfoEntity.getGestionnaire(), FontAwesomeIcon.USER);
         addInfo("Date de création :\n" + setDate(domaineInfoEntity.getDateCreation()), FontAwesomeIcon.CHECK_CIRCLE_ALT);
         addInfo("Date d'expiration :\n" + setDate(domaineInfoEntity.getDateExpiration()), FontAwesomeIcon.CALENDAR_TIMES_ALT);
@@ -50,6 +59,46 @@ public class DomaineNcInfoController implements Initializable {
         icon.getStyleClass().add("color");
         Label info = new Label(contenu);
         info.getStyleClass().add("color");
+        HBox container = new HBox(16);
+        container.setPadding(new Insets(8));
+        container.setPrefSize(300, 60);
+        container.getChildren().addAll(icon, info);
+        infoVbox.getChildren().add(container);
+    }
+
+    public void addInfoLien(String contenu, FontAwesomeIcon iconType) {
+        FontAwesomeIconView icon = new FontAwesomeIconView(iconType);
+        icon.setSize("28px");
+        icon.getStyleClass().add("color");
+        Label info = new Label(contenu);
+        info.getStyleClass().add("color");
+        info.setOnMouseClicked(event -> {
+            try {
+                openLink(event);
+            } catch (URISyntaxException | IOException e) {
+                e.printStackTrace();
+            }
+        });
+        HBox container = new HBox(16);
+        container.setPadding(new Insets(8));
+        container.setPrefSize(300, 60);
+        container.getChildren().addAll(icon, info);
+        infoVbox.getChildren().add(container);
+    }
+
+    public void addInfoLienRidet(String contenu, FontAwesomeIcon iconType) {
+        FontAwesomeIconView icon = new FontAwesomeIconView(iconType);
+        icon.setSize("28px");
+        icon.getStyleClass().add("color");
+        Label info = new Label(contenu);
+        info.getStyleClass().add("color");
+        info.setOnMouseClicked(event -> {
+            try {
+                openRidetEntreprise(event);
+            } catch (URISyntaxException | IOException e) {
+                e.printStackTrace();
+            }
+        });
         HBox container = new HBox(16);
         container.setPadding(new Insets(8));
         container.setPrefSize(300, 60);
@@ -90,4 +139,13 @@ public class DomaineNcInfoController implements Initializable {
         }
         return beneficiaire;
     }
+
+    public void openLink(MouseEvent event) throws URISyntaxException,IOException{
+        Desktop.getDesktop().browse(new URI("https://www.domaine.nc/whos?domain="+this.nom+"&ext=.nc"));
+    }
+
+    public void openRidetEntreprise(MouseEvent event) throws URISyntaxException,IOException{
+        Desktop.getDesktop().browse(new URI("https://data.gouv.nc/explore/dataset/entreprises-actives-au-ridet/table/?disjunctive.libelle_formjur&disjunctive.code_ape&disjunctive.libelle_naf&disjunctive.section_naf&disjunctive.libelle_section_naf&disjunctive.libelle_commune&disjunctive.hors_nc&disjunctive.province&q="+this.ridet));
+    }
+
 }
